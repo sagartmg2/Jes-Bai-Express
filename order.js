22f5f42333997e2cef7091fb4b9cdfc2c81a14fe
Sagar <dev.sagartmg@gmail.com>
2023-08-08 16:56:04 +0545

    check buyer middleware

---
+++ order.js
@@ -0,0 +1,49 @@
+/* {} */
+customers = [
+    {
+        name:abc
+    },
+    {
+        name:xzy
+    }
+]
+
+oders = [
+    {
+
+    },
+    {
+        
+        customer_name:"xyz",
+        customer_email:"xyz@gmail.com",
+        products:[
+            {
+                name:watch,
+                quanity:1 ,
+                price:1000
+            },
+            {
+                name:keyboard,
+                quanity:1 ,
+                price:1000
+            }
+        ]
+    },
+    {
+        customer_name:"abc",
+        customer_email:"abc@gmail.com",
+        phone:"1111",
+        products:[
+            {
+                name:watch,
+                quanity:2 ,
+                price:2000
+            },
+            {
+                name:keyboard,
+                quanity:1 ,
+                price:1000
+            }
+        ]
+    }
+]
--- products-api/index.js
+++ products-api/index.js
@@ -1,34 +1,52 @@
 const express = require("express")
 const app = express()
 
-
 /* middleware
     function which has access to req and res and also can modify them. 
 
     and also has access to next valid middleware
 */
 
+
 const checkAuthentication = (req, res, next) => {
     console.log("check authentication...")
-    let loggedIn = false;
+    let loggedIn = true;
     if (!loggedIn) {
         return res.status(401).send({ msg: "unauthenticated" })
     }
     next()
 }
 
+const checkBuyer = (req, res, next) => {
+    let role = "buyer"
+    console.log("check buyer..")
 
+    if (role === "buyer") {
+        next()
+    } else {
+        res.status(403).send({
+            msg: "forbidden"
+        })
+    }
+}
+
+app.use(express.json()) // global middleware // runs in ever route, sets up req.body 
+
+// app.use(checkBuyer) // global middelware
 // app.use(checkAuthentication) // global middleware 
 
-
 /* protected routes */
-
-
 app.get("/api/dashboard", checkAuthentication, (request, response) => { // route level middelware
     response.send("dashboard data.")
 })
-app.get("/api/orders", checkAuthentication, (request, response) => {
+
+app.get("/api/orders", checkAuthentication, checkBuyer, (request, response) => {
     response.send("order data.")
+})
+
+app.post("/api/orders", checkAuthentication, checkBuyer, function (request, response) {
+    console.log("req.body", request.body)
+    response.send("create order .")
 })
 
 app.get("/api/test", (request, response) => {
